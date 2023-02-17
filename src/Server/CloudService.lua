3361b2ae30b388c8845c95936408e0ae8c061750
OscarWoHA <oscar@halland.tech>
2023-02-17 20:15:08 +0100

    completed some additional work on CloudService

--- src/Server/CloudService.lua
+++ src/Server/CloudService.lua
@@ -10,18 +10,43 @@ local Promise = require(ReplicatedStorage.Styngr.Packages.promise)
 local CloudService = {}
 
 local ENVIRONMENT_VARIABLES = {
-    API_KEY = "",
-    API_ID = ""
+	API_KEY = "",
+	API_ID = "",
+    API_SERVER = "https://tst.api.styngr.com/api/sdk/"
 }
 
-local refreshToken
+--[[
+    Handles requesting a new token if there's none or if the current one is expired
+]]
+local function getToken()
+	return Promise.new(function(resolve, reject) end)
+end
 
 --[[
     Wraps around every call to external API, ensures token is refreshed and handles errors in a graceful-ish matter
 ]]
-function CloudService:Call()
-    return Promise.new(function(resolve, reject)
+function CloudService:Call(endpoint, method)
+    assert(endpoint and typeof(endpoint) == "string" and method and typeof(method) == "string")
 
+	return getToken():andThen(function(token)
+        return Promise.new(function(resolve, reject)
+            local request = {
+                Url = ENVIRONMENT_VARIABLES.API_SERVER .. endpoint,
+                Method = method,
+                Headers = {
+                    Authorization = "Bearer " .. token,
+                    Accept = "application/json"
+                },
+            }
+
+            local ok, result = pcall(HttpService.RequestAsync, HttpService, request)
+
+            if ok then
+                resolve(result)
+            else
+                reject(result)
+            end
+        end)
     end)
 end
 
@@ -29,14 +54,14 @@ end
     Initialization method
 ]]
 function CloudService:Init()
-    assert(HttpService.HttpEnabled, "Http requests needs to be enabled for Styngr to work!")
+	assert(HttpService.HttpEnabled, "Http requests needs to be enabled for Styngr to work!")
 end
 
 --[[
     Startup method
 ]]
 function CloudService:Start()
-    -- TODO: Do initial authentication, setup refresh token or cancel out if authentication fails
+	-- TODO: Do initial authentication, setup refresh token or cancel out if authentication fails
 end
 
 return CloudService
