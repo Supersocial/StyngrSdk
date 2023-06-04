123b56a6044740c891e141d896e7ff9111ae41e2
OscarWoHA <oscar@halland.tech>
2023-06-05 00:12:56 +0200

    feat: circular progress for when song is playing

---
+++ src/StarterPlayer/StarterPlayerScripts/Components/CircularProgress.lua
@@ -0,0 +1,124 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Fusion = require(ReplicatedStorage.Styngr.Packages.fusion)
+
+local New = Fusion.New
+local Children = Fusion.Children
+local Computed = Fusion.Computed
+
+local function CircularProgress(props)
+	local percentNumber = Computed(function()
+		return math.clamp(props.percentage:get() * 3.6, 0, 360)
+	end)
+
+	return New("Frame")({
+		Name = "Progress",
+		--AnchorPoint = Vector2.new(0.5, 0.5),
+		AnchorPoint = props.anchorPoint,
+		BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+		BackgroundTransparency = 1,
+		BorderColor3 = Color3.fromRGB(0, 0, 0),
+		BorderSizePixel = 0,
+		-- Position = UDim2.fromScale(0.5, 0.5),
+		-- Size = UDim2.fromScale(2.55, 2.55),
+		Position = props.position,
+		Size = props.size,
+		SizeConstraint = Enum.SizeConstraint.RelativeYY,
+
+		[Children] = {
+			New("Frame")({
+				Name = "Frame1",
+				BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+				BackgroundTransparency = 1,
+				BorderColor3 = Color3.fromRGB(0, 0, 0),
+				BorderSizePixel = 0,
+				ClipsDescendants = true,
+				Size = UDim2.fromScale(0.5, 1),
+
+				[Children] = {
+					New("ImageLabel")({
+						Name = "ImageLabel",
+						Image = "rbxassetid://3587367081",
+						ImageTransparency = 0,
+						ImageColor3 = Color3.fromRGB(255, 255, 255),
+						BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+						BackgroundTransparency = 1,
+						BorderColor3 = Color3.fromRGB(0, 0, 0),
+						BorderSizePixel = 0,
+						Size = UDim2.fromScale(2, 1),
+
+						[Children] = {
+							New("UIGradient")({
+								Name = "UIGradient",
+								Rotation = Computed(function()
+									return math.clamp(percentNumber:get(), 180, 360)
+								end),
+								Color = ColorSequence.new({
+									ColorSequenceKeypoint.new(0, props.progressColor),
+									ColorSequenceKeypoint.new(0.5, props.progressColor),
+									ColorSequenceKeypoint.new(0.501, Color3.fromRGB(0, 0, 0)),
+									ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
+								}),
+								Transparency = NumberSequence.new({
+									NumberSequenceKeypoint.new(0, 0),
+									NumberSequenceKeypoint.new(0.5, 0),
+									NumberSequenceKeypoint.new(0.501, 0.5),
+									NumberSequenceKeypoint.new(1, 0.5),
+								}),
+							}),
+						},
+					}),
+				},
+			}),
+
+			New("Frame")({
+				Name = "Frame2",
+				AnchorPoint = Vector2.new(1, 0),
+				BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+				BackgroundTransparency = 1,
+				BorderColor3 = Color3.fromRGB(0, 0, 0),
+				BorderSizePixel = 0,
+				ClipsDescendants = true,
+				Position = UDim2.fromScale(1, 0),
+				Size = UDim2.fromScale(0.5, 1),
+
+				[Children] = {
+					New("ImageLabel")({
+						Name = "ImageLabel",
+						Image = "rbxassetid://3587367081",
+						ImageTransparency = 0,
+						ImageColor3 = Color3.fromRGB(255, 255, 255),
+						BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+						BackgroundTransparency = 1,
+						BorderColor3 = Color3.fromRGB(0, 0, 0),
+						BorderSizePixel = 0,
+						Position = UDim2.fromScale(-1, 0),
+						Size = UDim2.fromScale(2, 1),
+
+						[Children] = {
+							New("UIGradient")({
+								Name = "UIGradient",
+								Rotation = Computed(function()
+									return math.clamp(percentNumber:get(), 0, 180)
+								end),
+								Color = ColorSequence.new({
+									ColorSequenceKeypoint.new(0, props.progressColor),
+									ColorSequenceKeypoint.new(0.5, props.progressColor),
+									ColorSequenceKeypoint.new(0.501, Color3.fromRGB(0, 0, 0)),
+									ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0)),
+								}),
+								Transparency = NumberSequence.new({
+									NumberSequenceKeypoint.new(0, 0),
+									NumberSequenceKeypoint.new(0.5, 0),
+									NumberSequenceKeypoint.new(0.501, 0.5),
+									NumberSequenceKeypoint.new(1, 0.5),
+								}),
+							}),
+						},
+					}),
+				},
+			}),
+		},
+	})
+end
+
+return CircularProgress
--- src/StarterPlayer/StarterPlayerScripts/Components/HUDButton.lua
+++ src/StarterPlayer/StarterPlayerScripts/Components/HUDButton.lua
@@ -11,10 +11,88 @@ local Computed = Fusion.Computed
 local OnEvent = Fusion.OnEvent
 
 local InterfaceStates = require(StarterPlayer.StarterPlayerScripts.Styngr.InterfaceStates)
+local CircularProgress = require(StarterPlayer.StarterPlayerScripts.Styngr.Components.CircularProgress)
+local ProgressState = require(StarterPlayer.StarterPlayerScripts.Styngr.ProgressState)
 
 local function HUDButton()
 	local closed = Computed(function()
 		return State:get().interfaceState == InterfaceStates.CLOSED
+	end)
+
+	local children = {
+		New("UIAspectRatioConstraint")({
+			Name = "UIAspectRatioConstraint",
+		}),
+
+		New("UICorner")({
+			Name = "UICorner",
+			CornerRadius = UDim.new(1, 0),
+		}),
+
+		New("UIPadding")({
+			Name = "UIPadding",
+			PaddingBottom = UDim.new(0.233, 0),
+			PaddingLeft = UDim.new(0.233, 0),
+			PaddingRight = UDim.new(0.233, 0),
+			PaddingTop = UDim.new(0.233, 0),
+		}),
+
+		New("ImageLabel")({
+			Visible = Computed(function()
+				return closed:get()
+			end),
+			Name = "Music",
+			Image = "rbxassetid://13219928845",
+			AnchorPoint = Vector2.new(0.5, 0.5),
+			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+			BackgroundTransparency = 1,
+			Position = UDim2.fromScale(0.5, 0.5),
+			Size = UDim2.fromScale(1, 1),
+		}),
+
+		New("ImageLabel")({
+			Visible = Computed(function()
+				return not closed:get()
+			end),
+			Name = "Cross",
+			Image = "rbxassetid://13220000054",
+			AnchorPoint = Vector2.new(0.5, 0.5),
+			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+			BackgroundTransparency = 1,
+			Position = UDim2.fromScale(0.5, 0.5),
+			Size = UDim2.fromScale(1, 1),
+		}),
+	}
+
+	local computedChildren = Computed(function()
+		local state = State:get()
+		local interfaceState = state.interfaceState
+		local nowPlaying = state.nowPlaying
+
+		if nowPlaying and interfaceState == InterfaceStates.CLOSED then
+			table.insert(
+				children,
+				New("UIStroke")({
+					Color = Color3.fromRGB(255, 255, 255),
+					Thickness = 2,
+					Transparency = 0.5,
+				})
+			)
+			table.insert(
+				children,
+				CircularProgress({
+					percentage = Computed(function()
+						return ProgressState:get() * 100
+					end),
+					progressColor = Color3.fromRGB(122, 247, 255),
+					size = UDim2.fromScale(2.56, 2.56),
+					anchorPoint = Vector2.new(0.5, 0.5),
+					position = UDim2.fromScale(0.5, 0.5),
+				})
+			)
+		end
+
+		return children
 	end)
 
 	return New("ImageButton")({
@@ -35,7 +113,7 @@ local function HUDButton()
 
 			return 0
 		end),
-		Position = UDim2.fromScale(0.5, 0.991),
+		Position = UDim2.fromScale(0.5, 0.98),
 		Selectable = false,
 		Size = UDim2.fromScale(1, 0.083),
 
@@ -58,50 +136,7 @@ local function HUDButton()
 			end)
 		end,
 
-		[Children] = {
-			New("UIAspectRatioConstraint")({
-				Name = "UIAspectRatioConstraint",
-			}),
-
-			New("UICorner")({
-				Name = "UICorner",
-				CornerRadius = UDim.new(1, 0),
-			}),
-
-			New("UIPadding")({
-				Name = "UIPadding",
-				PaddingBottom = UDim.new(0.233, 0),
-				PaddingLeft = UDim.new(0.233, 0),
-				PaddingRight = UDim.new(0.233, 0),
-				PaddingTop = UDim.new(0.233, 0),
-			}),
-
-			New("ImageLabel")({
-				Visible = Computed(function()
-					return closed:get()
-				end),
-				Name = "Music",
-				Image = "rbxassetid://13219928845",
-				AnchorPoint = Vector2.new(0.5, 0.5),
-				BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-				BackgroundTransparency = 1,
-				Position = UDim2.fromScale(0.5, 0.5),
-				Size = UDim2.fromScale(1, 1),
-			}),
-
-			New("ImageLabel")({
-				Visible = Computed(function()
-					return not closed:get()
-				end),
-				Name = "Cross",
-				Image = "rbxassetid://13220000054",
-				AnchorPoint = Vector2.new(0.5, 0.5),
-				BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-				BackgroundTransparency = 1,
-				Position = UDim2.fromScale(0.5, 0.5),
-				Size = UDim2.fromScale(1, 1),
-			}),
-		},
+		[Children] = computedChildren,
 	})
 end
 
